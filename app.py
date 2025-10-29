dde4c2f6650b4f7861181110c64c73d5fbdcc16f
Oliver Cheruiyot <olivercher1000@gmail.com>
2025-10-29 15:29:30 +0300

    print hello world with streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+# 1️⃣ Import the Streamlit library
+import streamlit as st  
+
+# 2️⃣ Create a title for the web app
+st.title("Hello, Streamlit! 👋")  
+
+# 3️⃣ Display a line of text below the title
+st.write("This is my first Streamlit app.")  
+
+# 4️⃣ Add an interactive button
+st.button("Click Me!")  
